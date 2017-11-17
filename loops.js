9847a9e774475dea497fff99dddaee250a410383
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-17 23:21:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(arr) {
   for (let i = 0; i < 25; i++) {
-    arr[i] = "I am ${i+1} strange loop${i === 0 ? "" : "s"}."
+    arr[i] = "I am ${i+1} strange loop${i === 0 ? '' : 's'}.";
   }
 }
