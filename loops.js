8e259b9e20ab85192a317b354bd35f2470b84dc5
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-17 23:41:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,13 @@
-
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 0) {
+      arr.push("I am 1 strange loop.");
+    }
+    else {
+      arr.push("I am ${i+1} strange loops.");
+    }
+  }
+}
 
 function whileLoop(n) {
   while (n > 0) {
